1f9e49adbb30111fc535629ed03db12f81340656
combipatal <124742777+combipatal@users.noreply.github.com>
2025-08-20 18:06:38 +0900

    FPGA TEST

--- 4bit_counter_7segment/counter_4bit_up.v
+++ 4bit_counter_7segment/counter_4bit_up.v
@@ -1,10 +1,12 @@
 module counter_4bit_up (clk,clr_n,seg_out);
 
 input clk,clr_n;
-output [7:0] seg_out;
+output [6:0] seg_out;
 wire [3:0] qout;
+wire clk_1hz;
 
-counter4 COUNT (.clk(clk),.clr_n(clr_n),.qout(qout));	
+clk_div CDIV(.clk(clk), .clr_n(clr_n), .clk_1hz(clk_1hz));
+counter4 COUNT (.clk(clk_1hz),.clr_n(clr_n),.qout(qout));	
 bin7seg SEG (.qout(qout), .seg_out(seg_out));
 
 endmodule
