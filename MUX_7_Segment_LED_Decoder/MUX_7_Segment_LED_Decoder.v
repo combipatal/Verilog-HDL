50344fcf753c41cf4bee7dd8c3de3b0510e6aa2e
combipatal <124742777+combipatal@users.noreply.github.com>
2025-08-20 20:33:58 +0900

    실 습 끝

--- MUX_7_Segment_LED_Decoder/MUX_7_Segment_LED_Decoder.v
+++ MUX_7_Segment_LED_Decoder/MUX_7_Segment_LED_Decoder.v
@@ -4,8 +4,9 @@ input clk,clr_n,select;
 output [6:0] seg_out;
 wire [3:0] Wup_count, Wdown_count, Wmux_out;
 
-up_counter_4bit UP1(.clk(clk), .clr_n(clr_n), .up_count(Wup_count));
-down_counter_4bit DOWN1(.clk(clk), .clr_n(clr_n), .down_count(Wdown_count));
+clk_div CDIV(.clk(clk), .clr_n(clr_n), .clk_1hz(clk_1hz));
+up_counter_4bit UP1(.clk(clk_1hz), .clr_n(clr_n), .up_count(Wup_count));
+down_counter_4bit DOWN1(.clk(clk_1hz), .clr_n(clr_n), .down_count(Wdown_count));
 mux_4bit_2x1 MUX(.up_count(Wup_count), .down_count(Wdown_count), .select(select), .mux_out(Wmux_out));
 bin7seg SEG(.mux_out(Wmux_out), .seg_out(seg_out));
 
